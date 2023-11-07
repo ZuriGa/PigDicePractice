ad1fe2e102e46970a655e6754344623297b19947
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-07 13:57:37 -0800

    initial test pass

--- __test__/pigDice.test.js
+++ __test__/pigDice.test.js
@@ -1,8 +1,10 @@
-import pigDice from "./../src/pigDice.js";
+import PigDiceGame from "./../src/pigDice.js";
 
 describe('PigDiceGame', () => {
     test('It should correctly create a Pig Dice object with two players, player1 and player2', () => {
-    
-
+        const player1 = new PigDiceGame("Player 1");
+        const player2 = new PigDiceGame("Player 2");
+        const game = new PigDiceGame([player1, player2]);
+        expect(game.players).toHaveLength(2);
     });
 });
--- src/pigDice.js
+++ src/pigDice.js
@@ -1,3 +1,5 @@
 export default function PigDiceGame(player1, player2) {
+    this.players = [{player1}, {player2}];
+    
 
 }
