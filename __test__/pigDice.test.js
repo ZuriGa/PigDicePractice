a9e31b9b74a6443d964ad2544ac92c24e7e1f30d
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-07 13:27:49 -0800

    first test passed

---
+++ __test__/pigDice.test.js
@@ -0,0 +1,8 @@
+import pigDice from "./../src/pigDice.js";
+
+describe('PigDiceGame', () => {
+    test('It should correctly create a Pig Dice object with two players, player1 and player2', () => {
+    
+
+    });
+});
---
+++ src/pigDice.js
@@ -0,0 +1,3 @@
+export default function PigDiceGame(player1, player2) {
+
+}
